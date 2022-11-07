05393bdfca233f59da394ca77c64ae86984b727a
Sebastian Gylstorff <seba124m@edu.zealand.dk>
2022-11-07 13:55:03 +0100

    update til vue template

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const app = Vue.createApp({
     data() {
         return {
             //properties here:
-            exampleName: 'eksempel',
-            næste: 'næste eksempel'
-        }
 
         }
+
+        },
+    
+    methods: {
+        calculate(){
+
+        }
+    },
+
     },
 );
